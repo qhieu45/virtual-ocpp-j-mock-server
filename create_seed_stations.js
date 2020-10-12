edb2f5d60202c0b949fe3f9f50319c824ca6e5bb
Hieu Cao <hieu.cao@virta.global>
2020-10-12 21:51:04 +0300

    Add more info to seed + custom routes for operations

--- create_seed_stations.js
+++ create_seed_stations.js
@@ -1,14 +1,20 @@
-const axios = require('axios');
+const axios = require("axios");
+const faker = require("faker");
 
-const url = 'http://localhost:3004'
+const url = "http://localhost:3004";
 
 const createNewStation = (identity) => {
-    axios.post(`${url}/stations`, {
-        identity
-    })
-}
+  axios.post(`${url}/stations`, {
+    identity,
+    centralSystemUrl: faker.internet.url(),
+    meterValue: faker.random.float(),
+    chargeInProgress: faker.random.boolean(),
+    currentTransactionId: faker.random.number(),
+    currentChargingPower: faker.random.number(500),
+  });
+};
 
 for (let i = 1; i <= 50; i++) {
-    const identity = `VIRTUAL-OCPPJ-STATION-${i}`
-    createNewStation(identity)
+  const identity = `CHARGEBOX_J_${i}`;
+  createNewStation(identity);
 }
---
+++ index.js
@@ -0,0 +1,64 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+server.use(function (req, res, next) {
+  setTimeout(() => {
+    next();
+  }, 1000);
+});
+
+// Add custom routes before JSON Server router
+server.post("/station/operations/:action", (req, res) => {
+  console.log("Body", req.body);
+  let status = "success";
+  let request;
+  let response;
+  switch (req.params.action) {
+    case "bootnotification":
+      request =
+        '[2,"G3XTH000-SN66-6888-2020-100718513315","BootNotification",{"chargePointModel":"IESG3X_Tri1","chargePointVendor":"IES_WANMA","firmwareVersion":"5.1A.06","chargeBoxSerialNumber":"G3XTH000002"}]';
+      response =
+        '[3,"G3XTH000-SN66-6888-2020-100700446059",{"status":"Accepted","currentTime":"2020-10-07T00:44:16.748188Z","interval":60}]';
+      break;
+    case "heartbeat":
+      request = '[2,"G3XTH000-SN66-6888-2020-100700006098","Heartbeat",{}]';
+      response =
+        '[3,"G3XTH000-SN66-6888-2020-100700006098",{"currentTime":"2020-10-07T00:00:14.140Z"}]';
+      break;
+    case "starttransaction":
+      request = `[2,"2","StartTransaction",{"timestamp":"2020-09-25T08:02:36Z","connectorId":1,"meterStart":100000,"idTag":"${req.body.idTag}"}]`;
+      response =
+        '[3,"a7caa7a1-c309-43bd-af3f-b5c1c4f9657e",{"idTagInfo":{"status":"Blocked","expiryDate":"2020-10-12T19:43:48Z"}}]';
+      break;
+    case "authorize":
+      request = `[2,"a7caa7a1-c309-43bd-af3f-b5c1c4f9657e","Authorize",{"idTag": "${req.body.idTag}"}]`;
+      response =
+        '[3,"2",{"transactionId":325845,"idTagInfo":{"status":"Accepted","expiryDate":"2020-10-09T14:04:56Z"}}]';
+      break;
+    case "stoptransaction":
+      request = `[2,"4","StopTransaction",{"timestamp":"2020-09-25T10:10:26Z","transactionId":${req.body.transactionId},"meterStop":260000,"idTag":"B5C99702","reason":"Local","transactionData":[{"timestamp":"2020-06-25T10:10:36Z","sampledValue":[{"value":"100.000","context":"Transaction.Begin","unit":"kWh"}]},{"timestamp":"2020-06-25T10:10:26Z","sampledValue":[{"value":"105.000","context":"Transaction.End","unit":"kWh"}]}]}]`;
+      response = `[3,"4",{"idTagInfo":{"status":"Accepted","expiryDate":"2020-10-09T14:48:27Z"}}]`;
+      break;
+    default:
+      status = "Not supported";
+      request = "";
+      response = "";
+  }
+  res.json({
+    status,
+    request,
+    response,
+  });
+});
+
+// Use default router
+server.use(router);
+server.listen(3004, () => {
+  console.log("JSON Server is running");
+});
